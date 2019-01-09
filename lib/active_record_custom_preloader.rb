e2d7487bc2c5dc4a438b2cdbf683284f03ff2d28
Denis Talakevich <senid231@gmail.com>
2019-01-09 18:26:29 +0200

    fix

--- lib/active_record_custom_preloader.rb
+++ lib/active_record_custom_preloader.rb
@@ -4,6 +4,7 @@ require 'active_record_custom_preloader/preload_with_options'
 require 'active_record_custom_preloader/associations_preloader'
 require 'active_record_custom_preloader/model_patch'
 require 'active_record_custom_preloader/relation_patch'
+require 'active_record_custom_preloader/with_multiple_foreign_keys_loading'
 
 module ActiveRecordCustomPreloader
   class Error < StandardError
