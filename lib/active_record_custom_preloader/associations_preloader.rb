8852cd02e67360c286f11ef52786896268396063
Denis Talakevich <senid231@gmail.com>
2021-08-16 17:22:33 +0300

    fix situation when same preloader being called multiple times

--- lib/active_record_custom_preloader/associations_preloader.rb
+++ lib/active_record_custom_preloader/associations_preloader.rb
@@ -23,11 +23,24 @@ module ActiveRecordCustomPreloader
 
     def preloaders_for_one(association, records, scope)
       if records.size > 0 && association.is_a?(PreloadWithOptions)
+        return if custom_loaded_all?(records, association)
+
         association.loader_for(records.first.class).preload(records)
         return
       end
 
       super
+    end
+
+    # ActiveRecord preloading mechanism does not make preloads list unique,
+    # so basically you can add same preload multiple times.
+    # In case all collection has custom value loaded, we skip preloading process.
+    # Same done at ActiveRecord::Associations::Preloader::AlreadyLoaded for associations.
+    # @param records [Array]
+    # @param association [ActiveRecordCustomPreloader::PreloadWithOptions]
+    # @return [Boolean]
+    def custom_loaded_all?(records, association)
+      records.all? { |record| record.has_custom_preloaded_value?(association.name) }
     end
   end
 end
